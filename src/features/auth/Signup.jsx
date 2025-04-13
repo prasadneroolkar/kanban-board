c702d73384c5abf097e3aa3ae51b4295a14b2b38
prasad <prasadneroolkar90@gmail.com>
2025-04-13 19:18:35 +0530

    learning tailwind installation

--- src/features/auth/Signup.jsx
+++ src/features/auth/Signup.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../services/firebase.js";
 
 const Signup = () => {
-  return <div></div>;
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("Login");
+  };
+
+  return (
+    <div className="mx-auto flex">
+      <form onSubmit={handleLogin}>
+        <label> Name </label>
+        <input type="text" />
+        <label> Email Id</label>
+        <input type="email" />
+        <label> Password</label>
+        <input type="password" />
+        <label> Confirm password</label>
+        <input type="password" />
+        <button type="Submit">create an account</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
