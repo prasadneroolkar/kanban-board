d45703ebdcbaba75ea0884fdfe1f0bd328b93f41
prasad <prasadneroolkar90@gmail.com>
2025-04-14 19:50:54 +0530

    learning tailwind installation

--- src/features/auth/Signup.jsx
+++ src/features/auth/Signup.jsx
@@ -9,16 +9,27 @@ const Signup = () => {
   };
 
   return (
-    <div className="mx-auto flex">
-      <form onSubmit={handleLogin}>
-        <label> Name </label>
-        <input type="text" />
-        <label> Email Id</label>
-        <input type="email" />
-        <label> Password</label>
-        <input type="password" />
-        <label> Confirm password</label>
-        <input type="password" />
+    <div className="mx-auto grid  lg:w-1/2 py-8 ">
+      <h1 className="text-center text-red-400 md:text-amber-200 sm:text-left lg:text-zinc-700 lg:custom-h2">
+        Signup
+      </h1>
+      <form onSubmit={handleLogin} className="grid">
+        <div>
+          <label> Names </label>
+          <input type="text" placeholder="Name" />
+        </div>
+        <div>
+          <label> Email Id</label>
+          <input type="email" placeholder="Email" />
+        </div>
+        <div>
+          <label> Password</label>
+          <input type="password" placeholder="Password" />
+        </div>
+        <div>
+          <label> Confirm password</label>
+          <input type="password" placeholder="Confirm Password" />
+        </div>
         <button type="Submit">create an account</button>
       </form>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/style.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
