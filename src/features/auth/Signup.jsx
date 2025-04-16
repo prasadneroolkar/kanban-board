7f87edd11262a1608fbc396f1cb91606a7fba244
prasad <prasadneroolkar90@gmail.com>
2025-04-16 20:06:34 +0530

    css in progress

--- src/features/auth/Signup.jsx
+++ src/features/auth/Signup.jsx
@@ -9,29 +9,38 @@ const Signup = () => {
   };
 
   return (
-    <div className="mx-auto grid  lg:w-1/2 py-8 ">
-      <h1 className="text-center text-red-400 md:text-amber-200 sm:text-left lg:text-zinc-700 lg:custom-h2">
-        Signup
-      </h1>
-      <form onSubmit={handleLogin} className="grid">
-        <div>
-          <label> Names </label>
-          <input type="text" placeholder="Name" />
+    <div className="bg-theme ">
+      <div className="mx-auto grid  lg:w-1/2 py-8 bg-primary ">
+        <div className="relative flex h-[50px] w-full my-[30px] justify-between border-1 border-gray-50 rounded-2xl">
+          <input type="radio" checked id="login" />
+          <input type="radio" id="signup" clasName="checked:left-5" />
+          <label className="size-full text-white text-lg font-medium">
+            login
+          </label>
+          <label>Signup</label>
+          <div className="absolute h-full w-1/2 left-0 z-0 rounded-[50px] bg-linear-to-r from-theme to-purple-300"></div>
         </div>
-        <div>
-          <label> Email Id</label>
-          <input type="email" placeholder="Email" />
-        </div>
-        <div>
-          <label> Password</label>
-          <input type="password" placeholder="Password" />
-        </div>
-        <div>
-          <label> Confirm password</label>
-          <input type="password" placeholder="Confirm Password" />
-        </div>
-        <button type="Submit">create an account</button>
-      </form>
+        <h1 className="text-center text-4xl tracking-wide">Signup</h1>
+        <form onSubmit={handleLogin} className="grid">
+          <div>
+            <label className="bg-mint-500"> Names </label>
+            <input type="text" placeholder="Name" />
+          </div>
+          <div>
+            <label> Email Id</label>
+            <input type="email" placeholder="Email" />
+          </div>
+          <div>
+            <label> Password</label>
+            <input type="password" placeholder="Password" />
+          </div>
+          <div>
+            <label> Confirm password</label>
+            <input type="password" placeholder="Confirm Password" />
+          </div>
+          <button type="Submit">create an account</button>
+        </form>
+      </div>
     </div>
   );
 };
