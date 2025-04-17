b290c6955f809a398ca6413f36f7d6f70e847410
prasad <prasadneroolkar90@gmail.com>
2025-04-17 20:29:15 +0530

    form in progress

--- src/features/auth/Signup.jsx
+++ src/features/auth/Signup.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../services/firebase.js";
 
 const Signup = () => {
+  const [loginChecked, setloginChecked] = useState(true);
+  const [signupChecked, setsignupChecked] = useState(false);
+
+  useEffect(() => {
+    console.log(loginChecked);
+    console.log(signupChecked);
+  }, [loginChecked, signupChecked]);
+
   const handleLogin = (event) => {
     event.preventDefault();
     console.log("Login");
@@ -10,36 +18,80 @@ const Signup = () => {
 
   return (
     <div className="bg-theme ">
-      <div className="mx-auto grid  lg:w-1/2 py-8 bg-primary ">
-        <div className="relative flex h-[50px] w-full my-[30px] justify-between border-1 border-gray-50 rounded-2xl">
-          <input type="radio" checked id="login" />
-          <input type="radio" id="signup" clasName="checked:left-5" />
-          <label className="size-full text-white text-lg font-medium">
+      <div className="mx-auto grid  lg:w-1/2 py-8 bg-primary max-w-[400px] p-[30px] overflow-hidden">
+        <div className="relative flex h-[50px] w-full my-[30px] justify-between border-1 border-gray-400 rounded-[50px] overflow-hidden">
+          <input
+            type="radio"
+            name="toggle"
+            defaultChecked
+            id="login"
+            className="hidden peer/login"
+            onChange={() => setloginChecked(!loginChecked)}
+          />
+          <input
+            type="radio"
+            name="toggle"
+            id="signup"
+            className=" hidden peer/signup"
+            onChange={() => setsignupChecked(!signupChecked)}
+          />
+          <label
+            htmlFor="login"
+            className="size-full capitalize text-base text-lg font-medium z-1 text-center leading-[48px] peer-checked/login:text-white duration-[0.6s] ease-initial"
+          >
             login
           </label>
-          <label>Signup</label>
-          <div className="absolute h-full w-1/2 left-0 z-0 rounded-[50px] bg-linear-to-r from-theme to-purple-300"></div>
+          <label
+            htmlFor="signup"
+            className="size-full capitalize text-base text-lg font-medium z-1 text-center leading-[48px] peer-checked/signup:text-white duration-[0.6s] ease-initial"
+          >
+            Signup
+          </label>
+          <div className="absolute h-full w-[50%] left-0 z-0 rounded-[50px] bg-linear-to-r from-theme to-purple-300 peer-checked/signup:left-1/2 peer-checked/login:left-0 duration-[0.6s] ease-in-expo"></div>
         </div>
-        <h1 className="text-center text-4xl tracking-wide">Signup</h1>
-        <form onSubmit={handleLogin} className="grid">
-          <div>
-            <label className="bg-mint-500"> Names </label>
-            <input type="text" placeholder="Name" />
-          </div>
-          <div>
-            <label> Email Id</label>
-            <input type="email" placeholder="Email" />
-          </div>
-          <div>
-            <label> Password</label>
-            <input type="password" placeholder="Password" />
-          </div>
-          <div>
-            <label> Confirm password</label>
-            <input type="password" placeholder="Confirm Password" />
-          </div>
-          <button type="Submit">create an account</button>
-        </form>
+        <div className="flex w-[200%] overflow-hidden">
+          {/* Login form */}
+          <form onSubmit={handleLogin} className="grid size-full">
+            <div>
+              <label className="bg-mint-500"> Names </label>
+              <input type="text" placeholder="Name" />
+            </div>
+            <div>
+              <label> Email Id</label>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div>
+              <label> Password</label>
+              <input type="password" placeholder="Password" />
+            </div>
+            <div>
+              <label> Confirm password</label>
+              <input type="password" placeholder="Confirm Password" />
+            </div>
+            <button type="Submit">create an account</button>
+          </form>
+
+          {/* Signup form */}
+          <form onSubmit={handleLogin} className="grid size-full">
+            <div>
+              <label className="bg-mint-500"> Names </label>
+              <input type="text" placeholder="Name" />
+            </div>
+            <div>
+              <label> Email Id</label>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div>
+              <label> Password</label>
+              <input type="password" placeholder="Password" />
+            </div>
+            <div>
+              <label> Confirm password</label>
+              <input type="password" placeholder="Confirm Password" />
+            </div>
+            <button type="Submit">create an account</button>
+          </form>
+        </div>
       </div>
     </div>
   );
