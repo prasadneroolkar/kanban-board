1f550494467361bc8738d1bbc410b4969a8765ae
prasad <prasadneroolkar90@gmail.com>
2025-05-04 11:45:30 +0530

    ui component in progress

--- src/components/board/Columns.jsx
+++ src/components/board/Columns.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TaskCard from "../board/TaskCard";
+import useColor from "../../hooks/useColor.js";
 
 const Columns = () => {
+  const { randomColor } = useColor();
+  // console.log("randomColor", randomColor());
+  const [first, setfirst] = useState();
+
+  const changeColor = () => {
+    const colorvalue = randomColor();
+    setfirst(colorvalue);
+  };
+
+  // useEffect(() => {}, []);
+
   return (
     <div className="min-w-[280px]">
       <h3 className="space-x-2 flex items-center">
-        <span className="rounded-full bg-green-500 w-4 h-4 inline-block"></span>
+        <span className={`rounded-full ${first} w-4 h-4 inline-block`}></span>
         <span className="text-dark-text tracking-widest font-semibold">
           Todo (2)
         </span>
       </h3>
       <TaskCard />
       <TaskCard />
+
+      <button type="button" onClick={changeColor}>
+        chnage
+      </button>
     </div>
   );
 };
--- src/hooks/useColor.js
+++ src/hooks/useColor.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const useColor = () => {
-  return <>Infosys@90</>;
+  const random = [
+    " bg-blue-500",
+    " bg-amber-500",
+    " bg-red-500",
+    " bg-green-600",
+    " bg-purple-500",
+  ];
+  const randomColor = () => {
+    const resultColor = Math.floor(Math.random() * random.length);
+
+    return random[resultColor];
+  };
+  const value = randomColor();
+  return { value, randomColor };
 };
 
 export default useColor;
